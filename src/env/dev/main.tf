37cec10b3fcb447c8ad783692e89b883dbe3e49b
Steven Carlson <steven.carlson007@gmail.com>
2021-07-17 23:31:54 -0500

    first add

---
+++ src/env/dev/main.tf
@@ -0,0 +1,11 @@
+resource "spotify_playlist" "playlist" {
+  name        = "Terraform Summer Playlist"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
